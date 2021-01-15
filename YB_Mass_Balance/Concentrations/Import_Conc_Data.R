a59e09ba642eeb5016d39490a7ca643c2fe71ee4
mountaindboz <53193874+mountaindboz@users.noreply.github.com>
2021-01-15 12:06:58 -0800

    Adding a Detect variable to the imported concentration data

--- YB_Mass_Balance/Concentrations/Import_Conc_Data.R
+++ YB_Mass_Balance/Concentrations/Import_Conc_Data.R
@@ -12,13 +12,18 @@ library(openwaterhg)
 temp_conc_clean <- conc_data %>% 
   # Remove samples with QualCode "R"
   filter(is.na(QualCode) | !str_detect(QualCode, "^R")) %>%
+  # Create a new variable Detect to indicate if the result was detected or below the detection limit
+  mutate(Detect = if_else(str_detect(Result, "^<"), "Non-detect", "Detect")) %>% 
   # Create a new variable Conc, which is a numeric version of Result with the MDL and RL for the ND values
   add_num_result() %>% 
   # Clean up df
-  select(SampleCode:Analyte, Conc, RL:QualCode)
+  select(SampleCode:Analyte, Conc, Detect, RL:QualCode)
+
+# Add Detect variable to particulate concentration data frame
+temp_part_conc <- part_conc_calc %>% mutate(Detect = "Detect")
 
 # Bind conc_data with calculated data for the particulate fractions of Hg, MeHg, and organic carbon
-all_conc <- bind_rows(temp_conc_clean, part_conc_calc)
+all_conc <- bind_rows(temp_conc_clean, temp_part_conc)
 
 # Clean up
-rm(temp_conc_clean)
+rm(temp_conc_clean, temp_part_conc)
